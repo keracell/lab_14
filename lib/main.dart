b30a30ac414ab7b42aafc7130d1354a51c3242a4
Agil Kerimov (IT) <AKerimov@azercell.com>
2025-04-22 14:56:23 +0400

    login splash signup

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,17 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:lab_14/screens/chat.dart';
+import 'package:lab_14/screens/splash.dart';
 
-void main() {
+import 'firebase_options.dart';
+import 'package:lab_14/screens/auth.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const App());
 }
 
@@ -10,16 +21,19 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'FlutterChat',
-      theme: ThemeData().copyWith(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color.fromARGB(255, 63, 17, 177),
+        title: 'FlutterChat',
+        theme: ThemeData().copyWith(
+          colorScheme: ColorScheme.fromSeed(
+              seedColor: const Color.fromARGB(255, 63, 17, 177)),
         ),
-      ),
-      home: Scaffold(
-        appBar: AppBar(title: const Text('FlutterChat')),
-        body: const Center(child: Text('Hello, world!')),
-      ),
-    );
+        home: StreamBuilder(
+            stream: FirebaseAuth.instance.authStateChanges(),
+            builder: (ctx, snapshop) {
+              if (snapshop.connectionState == ConnectionState.waiting) {
+                return const SplashScreen();
+              }
+              if (snapshop.hasData) return const ChatScreen();
+              return const AuthScreen();
+            }));
   }
 }
